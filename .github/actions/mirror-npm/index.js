1766868477ce8baa2894f42f949969c039dc59dd
Andre Staltz <andre@staltz.com>
2022-04-17 18:45:08 +0300

    draft implementation of mirror-npm

---
+++ .github/actions/mirror-npm/index.js
@@ -0,0 +1,60 @@
+import {execa} from 'execa';
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function forEachSourceRepo(octokit, org, cb) {
+  let count = 0
+  for await (const response of octokit.paginate.iterator(
+    octokit.rest.repos.listForOrg,
+    {org, type: 'sources'},
+  )) {
+    const page = response.data;
+    for (const repo of page) {
+      if (++count === 10) return
+      cb(repo);
+    }
+  }
+}
+
+async function run() {
+  try {
+    const token = core.getInput('token');
+    const org = core.getInput('org');
+    const owners = core
+      .getInput('pioneer')
+      .split(',')
+      .map((s) => s.trim());
+    const octokit = github.getOctokit(token);
+
+    console.log('Owners');
+    for (const owner of owners) {
+      console.log(owner);
+    }
+    console.log('\n');
+
+    console.log('Repos:');
+    await forEachSourceRepo(octokit, org, (repo) => {
+      console.log(repo.name);
+      const {exitCode, stdout} = execa('npm', ['info', repo.name, '--json']);
+      if (exitCode === 0) {
+        const pkg = JSON.parse(stdout);
+        console.log(`${pkg.name} is an npm package`);
+        const prevOwners = pkg.maintainers.map((s) => s.split(' ')[0]);
+        const rmOwners = prevOwners.filter((s) => !owners.includes(s));
+        const addOwners = owners.filter((s) => !prevOwners.includes(s));
+        for (const removable of rmOwners) {
+          console.log(`  npm owner rm ${removable} ${pkg.name}`);
+        }
+        for (const addable of addOwners) {
+          console.log(`  npm owner add ${addable} ${pkg.name}`);
+        }
+        console.log('\n');
+      }
+    });
+    console.log('\n');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
